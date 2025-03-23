fa8dd4c2dcdfd16cc838b0a63f63e3100df7e243
Pruthviraj Jadhav <pruthviraj.jadhav@nitorinfotech.com>
2025-03-23 11:34:50 +0530

    project-setup

---
+++ demo/src/main/java/com/PrototypeZ/demo/PrototypeZApplication.java
@@ -0,0 +1,13 @@
+package com.PrototypeZ.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypeZApplication.class, args);
+	}
+
+}
